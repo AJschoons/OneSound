f9cd1b4482b5412691365082c807eac7cf9e69ff
SchoonSauce <adamschoonmaker12@gmail.com>
2014-07-07 20:20:12 -0400

    Initial commit from local git

---
+++ OneSound/TestViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TestViewController.swift
+//  OneSound
+//
+//  Created by adam on 7/7/14.
+//  Copyright (c) 2014 Adam Schoonmaker. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
