f10e9e15f3cdd0666628ff9299b8f92a778a5794
SchoonSauce <adamschoonmaker12@gmail.com>
2014-07-14 14:32:44 -0400

    SideNavigation stores userCell upon creation of the cell

--- OneSound/SideNavigationViewController.swift
+++ OneSound/SideNavigationViewController.swift
@@ -65,7 +65,7 @@ extension SideNavigationViewController: UITableViewDataSource {
         if indexPath.row == 0 {
             // If the user cell
             cell = tableView.dequeueReusableCellWithIdentifier(userCellIdentifier, forIndexPath: indexPath) as SideNavigationUserCell
-            userCell = cell
+            userCell = (cell as SideNavigationUserCell)
         } else {
             // If a menu cell
             let menuCell = tableView.dequeueReusableCellWithIdentifier(menuCellIdentifier, forIndexPath: indexPath) as SideNavigationMenuCell
